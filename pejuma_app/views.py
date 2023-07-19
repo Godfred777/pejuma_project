c4cecd7c83846fc08fc7a6c64674aae8a7e47cc1
Godfred Opintan <godfredopintan77@gmail.com>
2023-07-19 19:49:41 +0000

    Makes the functionalities of the contact view

--- pejuma_app/views.py
+++ pejuma_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from .forms import ContactUsForm
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def home(request):
@@ -15,16 +16,16 @@ def signup(request):
 def signin(request):
     return render(request, 'pejuma_app/signin.html')
 
+@csrf_exempt
 def contact_us(request):
     try:
-        request = HttpRequest()
         if request.method == "POST":
             contact_form = ContactUsForm(request.POST)
             if contact_form.is_valid():
                 return HttpResponse('Success...')
         else:
             contact_form = ContactUsForm()
-        return render(request, 'pejuma_app/contactUs.html', {"form": contact_form}) 
+        return render(request, 'pejuma_app/contactUs.html', {"form":contact_form}) 
     except:
         return HttpResponse('Something went wrong')
 
